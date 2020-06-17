b5995253991d36ac641520d296b54f4f15e2ad2a
Anlisayuanyuan <342217736@qq.com>
2020-06-17 16:31:44 +0800

    trip登录接口

---
+++ trip_login.py
@@ -0,0 +1,26 @@
+# coding-=utf-8
+# @Time :   
+# @Author: 园园
+# @Email: 342217736@qq.com
+# @Software: PyCharm
+
+import requests
+url = 'http://47.98.200.110/api/Sign/SignIn'
+header = {
+                "ApplicationType": "ANDROID",
+                "userId": "55318",
+                "AppVersion": "3.8.3",
+                "Content-Type": "application/json;charset=utf-8",
+                "User-Agent": "okhttp/3.11.0"
+            }
+data = {
+                    "ApplicationType": "ANDROID",
+                    "Idfa": "865987043461583",
+                    "Mobile": "18667160715",
+                    "Password": "a1234567",
+                    "timestamp": "1592379887645"
+                }
+
+re = requests.post(url,headers=header,json=data)
+
+print(re.text)
